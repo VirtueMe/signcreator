03a43e33fd922f221f5643d851bb012f0b83a516
Benny Thomas <jan.thomas@speilbildet.com>
2017-03-31 01:38:03 +0200

    mapping emojis

---
+++ src/utils/imagemapper.js
@@ -0,0 +1,87 @@
+import f_fb from '../images/farger/far-blond4.png';
+import f_fme from '../images/farger/far-mellom4.png';
+import f_fmo from '../images/farger/far-mork4.png';
+
+import f_fbs from '../images/farger/far-blond-skjegg4.png';
+import f_fmes from '../images/farger/far-mellom-skjegg4.png';
+import f_fmos from '../images/farger/far-mork-skjegg4.png';
+
+import f_gb from '../images/farger/gutt-blond4.png';
+import f_gme from '../images/farger/gutt-mellom4.png';
+import f_gmo from '../images/farger/gutt-mork4.png';
+
+import f_jb from '../images/farger/jente-blond4.png';
+import f_jme from '../images/farger/jente-mellom4.png';
+import f_jmo from '../images/farger/jente-mork4.png';
+
+import f_mb from '../images/farger/mor-blond4.png';
+import f_mme from '../images/farger/mor-mellom4.png';
+import f_mmo from '../images/farger/mor-mork4.png';
+
+import i_bf from '../images/illustrasjoner/bestefar4.png';
+import i_bm from '../images/illustrasjoner/bestemor4.png';
+import i_f from '../images/illustrasjoner/far4.png';
+import i_m from '../images/illustrasjoner/mor4.png';
+import i_g from '../images/illustrasjoner/gutt4.png';
+import i_j from '../images/illustrasjoner/jente4.png';
+import i_b from '../images/illustrasjoner/baby4.png';
+import i_h from '../images/illustrasjoner/hund4.png';
+import i_k from '../images/illustrasjoner/katt4.png';
+import i_fi from '../images/illustrasjoner/fisk4.png';
+
+import s_df from '../images/standard/dame-farge4.png';
+import s_d from '../images/standard/dame4.png';
+import s_mf from '../images/standard/mann-farge4.png';
+import s_m from '../images/standard/mann4.png';
+
+import s_lgf from '../images/standard/gutt-liten-farge4.png';
+import s_lg from '../images/standard/gutt-liten4.png';
+import s_sgf from '../images/standard/gutt-stor-farge4.png';
+import s_sg from '../images/standard/gutt-stor4.png';
+import s_ljf from '../images/standard/jente-liten-farge4.png';
+import s_lj from '../images/standard/jente-liten4.png';
+import s_sjf from '../images/standard/jente-stor-farge4.png';
+import s_sj from '../images/standard/jente-stor4.png';
+
+
+const map2emoji = {
+  f_fb,
+  f_fme,
+  f_fmo,
+  f_fbs,
+  f_fmes,
+  f_fmos,
+  f_gb,
+  f_gme,
+  f_gmo,
+  f_jb,
+  f_jme,
+  f_jmo,
+  f_mb,
+  f_mme,
+  f_mmo,
+  i_bf,
+  i_bm,
+  i_f,
+  i_m,
+  i_g,
+  i_j,
+  i_b,
+  i_fi,
+  i_h,
+  i_k,
+  s_df,
+  s_d,
+  s_mf,
+  s_m,
+  s_lgf,
+  s_lg,
+  s_sgf,
+  s_sg,
+  s_ljf,
+  s_lj,
+  s_sjf,
+  s_sj
+};
+
+export { map2emoji };
