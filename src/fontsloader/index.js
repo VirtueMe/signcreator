59868f137e86f77536ddc56d42b8c77a43b51f34
Benny Thomas <jan.thomas@speilbildet.com>
2017-06-14 11:44:53 +0200

    show error messages

--- src/fontsloader/index.js
+++ src/fontsloader/index.js
@@ -1,17 +1,22 @@
 const FontFaceObserver = require('fontfaceobserver');
 
 
-const fonts = ['Cabin Condensed', 'Vidaloka', 'Neucha', 'Lobster']
+const fonts = ['Cabin Condensed', 'Lobster', 'Neucha', 'Vidaloka']
                 .map((name) => new FontFaceObserver(name))
                 .map((f) => { return f.load(); });
 
 var fontsLoaded = false;
 
 function fontsLoader(cb) {
-  return Promise.all(fonts).then(function () {
-    fontsLoaded = true;
-    return cb();
-  });
+  return Promise
+          .all(fonts)
+          .then(function () {
+            fontsLoaded = true;
+            return cb();
+          })
+          .catch(() => {
+            console.info(arguments);
+          });
 }
 
 function whenFontsLoaded(cb) {
